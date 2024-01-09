a996a6462838844de83baedbc1b40b0fe5ff1b78
Esme199 <150774630+Esme199@users.noreply.github.com>
2024-01-08 23:22:16 -0500

    GataBotLite-MD ~ Gata Dios

--- config.js
+++ config.js
@@ -20,10 +20,10 @@ global.owner = ["9837329484", '𝙊𝙬𝙣𝙚𝙧 💻🐈', true], //ESMERALD
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = "+529837329484" //Ejemplo: +59309090909
-global.confirmCode = ""
+global.confirmCode = "+529837329484"
 
-global.mods = []
-global.prems = []
+global.mods = [9837329484]
+global.prems = [9831913235]
 //key de violetics
 global.Key360 = ["964f-0c75-7afc"]
    
